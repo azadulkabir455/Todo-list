7df762eec1764e0705d0592ceb821280791df685
Azad ul kabir <azadulkabir10@gmail.com>
2021-04-21 01:32:13 +0600

    All DOM element created and append

---
+++ app.js
@@ -0,0 +1,58 @@
+let mainTodoContainer = document.querySelector('#todos');
+let inputData = document.querySelector('#todo-input-data');
+let addButton = document.querySelector('#add-data');
+let dltButton = document.querySelector('#delete-data');
+
+addButton.addEventListener('click', function(){
+    if(inputData.value.trim()) {
+
+        // Initial Tag create
+        let ulTag = document.createElement('ul');
+        ulTag.classList.add("todo-list-container");
+
+        
+        let todoList = document.createElement('div');
+        todoList.classList.add('todo-list');
+
+        
+        let liTag = document.createElement('li');
+        liTag.classList.add('todo-item');
+        liTag.innerHTML = inputData.value;
+
+
+        // Initial Button create
+
+        let buttonDiv = document.createElement('div');
+        buttonDiv.classList.add('button-group');
+
+        let completeButton = document.createElement('button');
+        completeButton.classList.add('complete');
+        completeButton.innerHTML = '<span>Complete</span>';
+
+        let editButton = document.createElement('button');
+        editButton.classList.add('edit');
+        editButton.innerHTML = '<span>Edit</span>';
+
+        let trashButton = document.createElement('button');
+        trashButton.classList.add('trash');
+        trashButton.innerHTML = '<span>Trash</span>';
+
+        // Append classes
+
+        ulTag.appendChild(todoList);
+        todoList.appendChild(liTag);
+        todoList.appendChild(buttonDiv);
+        buttonDiv.appendChild(completeButton);
+        buttonDiv.appendChild(editButton);
+        buttonDiv.appendChild(trashButton);
+
+        // Append for main div
+  
+        mainTodoContainer.appendChild(ulTag);
+        console.log(mainTodoContainer);
+
+        // When add input the input box is empty
+        inputData.value = "";
+        
+    }
+});
